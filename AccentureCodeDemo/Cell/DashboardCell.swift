f3caecd66d685884177a11ca8ce869b0f47a406b
ajeetzone.ios <ajeetzone.ios@gmail.com>
2020-04-21 22:17:27 +0530

    UI Design is done by Anchor layout. Xib, Storyboard removed, Comments removed, Warning resolved, MVVM pattern followed

--- AccentureCodeDemo/Cell/DashboardCell.swift
+++ AccentureCodeDemo/Cell/DashboardCell.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class DashboardCell: UITableViewCell {
+
+    private let imgvw = UIImageView()
+    private let lblTitle = UILabel()
+    private let lblDescription = UILabel()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.imageView?.setRoundCornder(0.6, .black)
-        self.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        self.detailTextLabel?.textColor = .darkGray
-        self.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
-        self.detailTextLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
-        self.textLabel?.numberOfLines = 0
-        self.detailTextLabel?.numberOfLines = 0
+        imgvw.setRoundCornder(0.6, .black)
+        lblTitle.font = UIFont.boldSystemFont(ofSize: 16)
+        lblDescription.textColor = .darkGray
+        lblTitle.lineBreakMode = NSLineBreakMode.byWordWrapping
+        lblDescription.lineBreakMode = NSLineBreakMode.byWordWrapping
+        lblTitle.numberOfLines = 0
+        lblDescription.numberOfLines = 0
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,17 +31,52 @@ class DashboardCell: UITableViewCell {
     }
     
     func configureCell(with item:Row) {
-        self.textLabel?.text = item.title ?? "N/A"
-        self.detailTextLabel?.text = item.description ?? "N/A"
+        lblTitle.text = item.title ?? "N/A"
+        lblDescription.text = item.description ?? "N/A"
         
         if let strImgURL = item.imageHref {
-            self.imageView?.setImageFromAlmofireURL(strImgURL: strImgURL, isShowIndicator: true, placeholderimg: Placeholder.noImage, completionHandler: { (isSuccess) in
+            imgvw.setImageFromAlmofireURL(strImgURL: strImgURL, isShowIndicator: true, placeholderimg: Placeholder.noImage, completionHandler: { (isSuccess) in
                 debugPrint("image Downloaded successfully")
             })
 
         }
     }
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(imgvw)
+        contentView.addSubview(lblTitle)
+        contentView.addSubview(lblDescription)
+
+        imgvw.translatesAutoresizingMaskIntoConstraints = false
+        imgvw.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor).isActive = true
+        imgvw.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        imgvw.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
+        imgvw.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
+        let heightConstraint = imgvw.heightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6)
+        heightConstraint.priority = UILayoutPriority.defaultLow
+        heightConstraint.isActive = true
+
+        lblTitle.translatesAutoresizingMaskIntoConstraints = false
+        lblTitle.topAnchor.constraint(equalTo: imgvw.safeAreaLayoutGuide.topAnchor).isActive = true
+        lblTitle.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
+        lblTitle.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
+        lblTitle.numberOfLines = 0
+
+        lblDescription.translatesAutoresizingMaskIntoConstraints = false
+        lblDescription.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor).isActive = true
+        lblDescription.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
+        lblDescription.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
+        lblDescription.bottomAnchor.constraint(equalTo: imgvw.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        lblDescription.numberOfLines = 0
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 
--- AccentureCodeDemo/ViewController/ViewController.swift
+++ AccentureCodeDemo/ViewController/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         let table = UITableView()
         table.separatorColor = .lightGray
 
-        table.register(DashboardCell2.self, forCellReuseIdentifier: CellManager.cellIdentifier)
+        table.register(DashboardCell.self, forCellReuseIdentifier: CellManager.cellIdentifier)
         
         table.dataSource = self
         table.delegate = self
@@ -134,7 +134,7 @@ extension TableviewConfiguration: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellManager.cellIdentifier) as? DashboardCell2 else {return UITableViewCell()}
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellManager.cellIdentifier) as? DashboardCell else {return UITableViewCell()}
         if arrInfo.count > 0 {
             let item = arrInfo[indexPath.row]
             cell.configureCell(with: item)
@@ -151,74 +151,4 @@ extension TableviewConfiguration: UITableViewDelegate {
 }
 
 
-class DashboardCell2: UITableViewCell {
 
-    private let imgvw = UIImageView()
-    private let lblTitle = UILabel()
-    private let lblDescription = UILabel()
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        imgvw.setRoundCornder(0.6, .black)
-        lblTitle.font = UIFont.boldSystemFont(ofSize: 16)
-        lblDescription.textColor = .darkGray
-        lblTitle.lineBreakMode = NSLineBreakMode.byWordWrapping
-        lblDescription.lineBreakMode = NSLineBreakMode.byWordWrapping
-        lblTitle.numberOfLines = 0
-        lblDescription.numberOfLines = 0
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    func configureCell(with item:Row) {
-        lblTitle.text = item.title ?? "N/A"
-        lblDescription.text = item.description ?? "N/A"
-        
-        if let strImgURL = item.imageHref {
-            imgvw.setImageFromAlmofireURL(strImgURL: strImgURL, isShowIndicator: true, placeholderimg: Placeholder.noImage, completionHandler: { (isSuccess) in
-                debugPrint("image Downloaded successfully")
-            })
-
-        }
-    }
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        contentView.addSubview(imgvw)
-        contentView.addSubview(lblTitle)
-        contentView.addSubview(lblDescription)
-
-        imgvw.translatesAutoresizingMaskIntoConstraints = false
-        imgvw.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor).isActive = true
-        imgvw.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        imgvw.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
-        imgvw.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
-        let heightConstraint = imgvw.heightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6)
-        heightConstraint.priority = UILayoutPriority.defaultLow
-        heightConstraint.isActive = true
-
-        lblTitle.translatesAutoresizingMaskIntoConstraints = false
-        lblTitle.topAnchor.constraint(equalTo: imgvw.safeAreaLayoutGuide.topAnchor).isActive = true
-        lblTitle.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
-        lblTitle.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
-        //lblTitle.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        lblTitle.numberOfLines = 0
-
-        lblDescription.translatesAutoresizingMaskIntoConstraints = false
-        lblDescription.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor).isActive = true
-        lblDescription.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor).isActive = true
-        lblDescription.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor).isActive = true
-        lblDescription.bottomAnchor.constraint(equalTo: imgvw.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        lblDescription.numberOfLines = 0
-
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
